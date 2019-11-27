132e21d9b147c2c0ed74807d5f839c76f0a96728
reinali07 <56851732+reinali07@users.noreply.github.com>
2019-11-27 11:59:05 -0500

    Update key_ISR.s

--- Lab_10/skeletoncode/key_ISR.s
+++ Lab_10/skeletoncode/key_ISR.s
@@ -1,10 +1,11 @@
+.include "address_map_arm.s"
 /***************************************************************************************
  * Pushbutton - Interrupt Service Routine                                
  *                                                                          
  * This routine checks which KEY has been pressed.  If KEY3 it stops/starts the timer.
 ****************************************************************************************/
 					.global	KEY_ISR
-KEY_ISR: 		LDR		R0, =0xFF200050			// base address of KEYs parallel port
+KEY_ISR: 		LDR		R0, =KEY_BASE			// base address of KEYs parallel port
 				LDR		R1, [R0, #0xC]			// read edge capture register
 				STR		R1, [R0, #0xC]			// clear the interrupt
 
@@ -14,7 +15,7 @@ CHK_KEY3:		MOV R3,#0b1000
                 
 UNEXPECTED_KEY:	BNE UNEXPECTED_KEY
 
-START_STOP:		LDR		R0, =0xFFFEC600		// timer base address
+START_STOP:		LDR		R0, =MPCORE_PRIV_TIMER		// timer base address
 				LDR		R1, [R0, #0x8]			// read timer control register
 				MOV 	R3,#1
 				EOR 	R1,R1,R3
@@ -22,4 +23,3 @@ START_STOP:		LDR		R0, =0xFFFEC600		// timer base address
 
 END_KEY_ISR:	MOV	PC, LR
 					.end
-	
